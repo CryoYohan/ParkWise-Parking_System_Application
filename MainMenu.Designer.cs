f38b4f1f51e47dd33071b721336e1253b0b698cc
itsmecyril <cyrilypil@gmail.com>
2024-03-01 15:21:48 +0800

    Cyril John Ypil

---
+++ MainMenu.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ParkingSystemGUI
+{
+    partial class MainMenu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            pictureBox1 = new PictureBox();
+            proceedButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(938, 639);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // proceedButton
+            // 
+            proceedButton.BackColor = Color.FromArgb(32, 105, 158);
+            proceedButton.Cursor = Cursors.Hand;
+            proceedButton.FlatAppearance.BorderSize = 0;
+            proceedButton.FlatStyle = FlatStyle.Flat;
+            proceedButton.Font = new Font("STXihei", 21F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            proceedButton.ForeColor = Color.FromArgb(238, 231, 231);
+            proceedButton.Location = new Point(357, 516);
+            proceedButton.Name = "proceedButton";
+            proceedButton.Size = new Size(251, 70);
+            proceedButton.TabIndex = 1;
+            proceedButton.Text = "PROCEED";
+            proceedButton.UseVisualStyleBackColor = false;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(126, 216, 253);
+            Controls.Add(proceedButton);
+            Controls.Add(pictureBox1);
+            Name = "MainMenu";
+            Size = new Size(939, 695);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button proceedButton;
+    }
+}
